2fe7f927bef2fb6409a3a6869c8321713d87e9ab
Job Muturi <muirimuturi17@gmail.com>
2023-07-05 11:17:55 +0300

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,49 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+User.create(
+  contact: "john@example.com",
+  location: "New York",
+  bio: "Experienced chef with a passion for cooking.",
+  username: "johndoe",
+  password: "password",
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaEdNNSvf5Trfnucfdsfw1laaKmt3_lH6v0g&usqp=CAU"
+)
+
+User.create(
+  contact: "jane@example.com",
+  location: "Los Angeles",
+  bio: "Creative chef specializing in fusion cuisine.",
+  username: "janesmith",
+  password: "password",
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaEdNNSvf5Trfnucfdsfw1laaKmt3_lH6v0g&usqp=CAU"
+)
+
+User.create(
+  contact: "michael@example.com",
+  location: "Chicago",
+  bio: "Gourmet chef with a focus on French cuisine.",
+  username: "michaeljohnson",
+  password: "password",
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTewu0DWuJHsd7YgCMfacBtpwwHanaN5Rz7Ug&usqp=CAU"
+)
+
+User.create(
+  contact: "emily@example.com",
+  location: "San Francisco",
+  bio: "Plant-based chef passionate about healthy cooking.",
+  username: "emilywilson",
+  password: "password",
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_pjMsX-nsV2BovsstTtWM0cBmqW4eDY_Odg&usqp=CAU"
+)
+
+User.create(
+  contact: "david@example.com",
+  location: "London",
+  bio: "Master sushi chef with years of experience.",
+  username: "davidlee",
+  password: "password",
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZtAz5l45T_VWC04mUqsxKgCfuc-01yeC5xQ&usqp=CAU"
+)
