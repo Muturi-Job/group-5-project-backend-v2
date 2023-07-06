9f87c2b12a507f5530a1452fb4f52100254aeb22
Job Muturi <muirimuturi17@gmail.com>
2023-07-06 12:26:19 +0300

    Fixed app config

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,13 @@ module ReactRailsApiProjectTemplate
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'  # You can restrict this to specific origins if needed
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
+    
 
     # Configuration for the application, engines, and railties goes here.
     #
